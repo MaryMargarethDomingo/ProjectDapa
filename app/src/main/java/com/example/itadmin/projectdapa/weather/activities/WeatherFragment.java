98a8de46b04e000a72d12abd28d9eaa52195bda4
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2018-02-13 16:28:27 +0800

    removed some lines of code

--- app/src/main/java/com/example/itadmin/projectdapa/weather/activities/WeatherFragment.java
+++ app/src/main/java/com/example/itadmin/projectdapa/weather/activities/WeatherFragment.java
@@ -264,37 +264,6 @@ public class WeatherFragment extends Fragment implements LocationListener {
         }
     }
 
-    private void aboutDialog() {
-        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
-        alert.setTitle("Forecastie");
-        final WebView webView = new WebView(getActivity());
-        String about = "<p>1.6.1</p>" +
-                "<p>A lightweight, opensource weather app.</p>" +
-                "<p>Developed by <a href='mailto:t.martykan@gmail.com'>Tomas Martykan</a></p>" +
-                "<p>Data provided by <a href='https://openweathermap.org/'>OpenWeatherMap</a>, under the <a href='http://creativecommons.org/licenses/by-sa/2.0/'>Creative Commons license</a>" +
-                "<p>Icons are <a href='https://erikflowers.github.io/weather-icons/'>Weather Icons</a>, by <a href='http://www.twitter.com/artill'>Lukas Bischoff</a> and <a href='http://www.twitter.com/Erik_UX'>Erik Flowers</a>, under the <a href='http://scripts.sil.org/OFL'>SIL OFL 1.1</a> licence.";
-        TypedArray ta = getActivity().obtainStyledAttributes(new int[]{android.R.attr.textColorPrimary, R.attr.colorAccent});
-        String textColor = String.format("#%06X", (0xFFFFFF & ta.getColor(0, Color.BLACK)));
-        String accentColor = String.format("#%06X", (0xFFFFFF & ta.getColor(1, Color.BLUE)));
-        ta.recycle();
-        about = "<style media=\"screen\" type=\"text/css\">" +
-                "body {\n" +
-                "    color:" + textColor + ";\n" +
-                "}\n" +
-                "a:link {color:" + accentColor + "}\n" +
-                "</style>" +
-                about;
-        webView.setBackgroundColor(Color.TRANSPARENT);
-        webView.loadData(about, "text/html", "UTF-8");
-        alert.setView(webView, 32, 0, 32, 0);
-        alert.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-
-            }
-        });
-        alert.show();
-    }
-
     private String setWeatherIcon(int actualId, int hourOfDay) {
         int id = actualId / 100;
         String icon = "";
