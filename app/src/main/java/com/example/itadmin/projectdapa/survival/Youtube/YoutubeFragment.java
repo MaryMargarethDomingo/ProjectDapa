d2b81edca409d75acb5a3378529a4821fbe21e38
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2018-02-18 16:32:43 +0800

    added youtube player

--- app/src/main/java/com/example/itadmin/projectdapa/survival/Youtube/YoutubeFragment.java
+++ app/src/main/java/com/example/itadmin/projectdapa/survival/Youtube/YoutubeFragment.java
@@ -1,53 +1,74 @@
 package com.example.itadmin.projectdapa.survival.Youtube;
 
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.itadmin.projectdapa.MainActivity;
 import com.example.itadmin.projectdapa.R;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeFragment extends Fragment {
 
-    private YouTubePlayerView youtubePlayerView;
-    private YouTubePlayer.OnInitializedListener onInitializedListener;
+    private YouTubePlayer YPlayer;
+    private static final String DEVELOPER_KEY = "AIzaSyBvSLp9j1nAV4CPRprUZkWLO-1_Gt_45Xk";
 
     Button button;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_youtube_layout, container, false);
+    public void onAttach(Activity activity) {
+        if (activity instanceof FragmentActivity) {
+            MainActivity myContext = (MainActivity) activity;
+        }
+
+        super.onAttach(activity);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        youtubePlayerView = getView().findViewById(R.id.youtube_view);
-        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+        View rootView = inflater.inflate(R.layout.fragment_youtube_layout, container, false);
+
+        YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtubeplayerfragment, youTubePlayerFragment).commit();
+
+        youTubePlayerFragment.initialize("DEVELOPER_KEY", new OnInitializedListener() {
+
             @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("Iuea4NzqMfI");
-                youtubePlayerView.initialize("AIzaSyBvSLp9j1nAV4CPRprUZkWLO-1_Gt_45Xk", onInitializedListener);
+            public void onInitializationSuccess(Provider arg0, YouTubePlayer youTubePlayer, boolean b) {
+                if (!b) {
+                    YPlayer = youTubePlayer;
+                    YPlayer.setFullscreen(false);
+                    YPlayer.loadVideo("Iuea4NzqMfI");
+                    YPlayer.play();
+                }
             }
 
             @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+            public void onInitializationFailure(Provider arg0, YouTubeInitializationResult arg1) {
+                // TODO Auto-generated method stub
 
-            }
-        };
-        button = getView().findViewById(R.id.btnPlay);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                youtubePlayerView.initialize("AIzaSyBvSLp9j1nAV4CPRprUZkWLO-1_Gt_45Xk", onInitializedListener);
             }
         });
+        return rootView;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 
 }
