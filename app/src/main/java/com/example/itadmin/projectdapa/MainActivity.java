ef1348d4a522a03e8fafa1aab48874c69067b6dc
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2018-03-08 13:47:12 +0800

    added navigation drawer/hamburger

--- app/src/main/java/com/example/itadmin/projectdapa/MainActivity.java
+++ app/src/main/java/com/example/itadmin/projectdapa/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements AHBottomNavigatio
     private MapsFragment mapsFragment = new MapsFragment();
     private SurvivalFragment survivalFragment = new SurvivalFragment();
     private int tabPosition;
+    private DrawerLayout mDrawerLayout;
 
 
     @Override
@@ -38,6 +41,24 @@ public class MainActivity extends AppCompatActivity implements AHBottomNavigatio
         setContentView(R.layout.activity_main);
 
         contextOfApplication = getApplicationContext();
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
 
         //if not logged in, go to loginActivity
         if (mAuth.getCurrentUser() == null) {
