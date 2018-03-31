d51eb017208cfa4222531668c20d93006efa9278
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2018-03-31 23:21:17 +0800

    Oops di pa pala namerge lahat kanina. MERGE v2.0

--- app/src/main/java/com/example/itadmin/projectdapa/survival/controller/survivalWebPages/WebViewContentFragment.java
+++ app/src/main/java/com/example/itadmin/projectdapa/survival/controller/survivalWebPages/WebViewContentFragment.java
@@ -12,31 +12,7 @@ import com.example.itadmin.projectdapa.R;
 public class WebViewContentFragment extends Fragment {
 
     WebView webView;
-<<<<<<< HEAD:app/src/main/java/com/example/itadmin/projectdapa/survival/controller/survivalWebPages/WebViewFloodFragment.java
-
-    String fileName = "";
-
-    public WebViewFloodFragment(){}
-
-    @SuppressLint("ValidFragment")
-    public WebViewFloodFragment(int position){
-        switch (position){
-            case 0:
-                fileName = "flood_before.html";
-                break;
-
-            case 1:
-                fileName = "flood_during.html";
-                break;
-
-            case 2:
-                fileName = "flood_after.html";
-                break;
-        }
-    }
-=======
     private static String args;
->>>>>>> e129ece1ad1532fc14b89ca1f4a988e7c5b2b45b:app/src/main/java/com/example/itadmin/projectdapa/survival/controller/survivalWebPages/WebViewContentFragment.java
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -52,11 +28,6 @@ public class WebViewContentFragment extends Fragment {
         super.onStart();
 
         webView = getView().findViewById(R.id.webview1);
-<<<<<<< HEAD:app/src/main/java/com/example/itadmin/projectdapa/survival/controller/survivalWebPages/WebViewFloodFragment.java
-        webView.loadUrl("file:///android_asset/" + fileName);
-=======
         webView.loadUrl("file:///android_asset/" + args);
-
->>>>>>> e129ece1ad1532fc14b89ca1f4a988e7c5b2b45b:app/src/main/java/com/example/itadmin/projectdapa/survival/controller/survivalWebPages/WebViewContentFragment.java
     }
 }
--- tweet-composer/build/generated/source/buildConfig/debug/com/twitter/sdk/android/tweetcomposer/BuildConfig.java
+++ tweet-composer/build/generated/source/buildConfig/debug/com/twitter/sdk/android/tweetcomposer/BuildConfig.java
@@ -13,7 +13,6 @@ public final class BuildConfig {
   // Fields from build type: debug
   public static final String ARTIFACT_ID = "tweet-composer";
   public static final String BUILD_NUMBER = "dev";
-
-  public static final long BUILD_TIME = 1522504989575L;
+  public static final long BUILD_TIME = 1522509490516L;
   public static final String GROUP = "com.twitter.sdk.android";
 }
