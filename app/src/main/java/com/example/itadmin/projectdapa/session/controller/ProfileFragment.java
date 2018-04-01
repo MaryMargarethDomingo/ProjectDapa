ea5178757ce64b8c22d1c0e2817e485543fe4613
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2018-04-01 19:02:21 +0800

    Profile page can now display user display image

--- app/src/main/java/com/example/itadmin/projectdapa/session/controller/ProfileFragment.java
+++ app/src/main/java/com/example/itadmin/projectdapa/session/controller/ProfileFragment.java
@@ -1,21 +1,37 @@
 package com.example.itadmin.projectdapa.session.controller;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.itadmin.projectdapa.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class ProfileFragment extends Fragment {
     private TextView email;
     private TextView phone;
     private TextView username;
+    private ImageView image;
+    private Button editProfile;
+    private Button logOut;
 
     @Override
     public void onStart(){
@@ -26,10 +42,39 @@ public class ProfileFragment extends Fragment {
         username = (TextView) getView().findViewById(R.id.username);
         email = (TextView) getView().findViewById(R.id.email);
         phone = (TextView) getView().findViewById(R.id.phone);
+        image = getView().findViewById(R.id.profilePic);
+        editProfile = getView().findViewById(R.id.editProfile);
+        logOut = getView().findViewById(R.id.logOut);
+
 
         username.setText(user.getDisplayName());
         email.setText(user.getEmail());
         phone.setText(user.getPhoneNumber());
+
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            if(user.getPhotoUrl() != null) {
+                try {
+                    image.setImageBitmap(BitmapFactory.decodeStream(new URL(user.getPhotoUrl().toString()).openConnection().getInputStream()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
     }
 
     @Override
