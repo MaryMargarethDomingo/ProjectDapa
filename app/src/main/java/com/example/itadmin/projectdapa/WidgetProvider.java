9e09dfbe3d5228d0fbe12e04ca8371750d779613
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2018-04-19 12:58:38 +0800

    added (not yet functioning) widget

---
+++ app/src/main/java/com/example/itadmin/projectdapa/WidgetProvider.java
@@ -0,0 +1,29 @@
+package com.example.itadmin.projectdapa;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Created by Michael on 19 Apr 2018.
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    // Retrieve the widget’s layout//
+    RemoteViews views = new RemoteViews(MainActivity.getContextOfApplication().getPackageName(), R.layout.appwidget);
+    private static final String onClickTag = "onClickTag";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+}
