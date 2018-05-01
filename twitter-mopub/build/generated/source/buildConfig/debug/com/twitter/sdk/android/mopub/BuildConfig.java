ad01514d7d00e1c49cc0d5fd431c51bd1a884996
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2018-05-01 10:55:51 +0800

    widget onclick now working

--- twitter-mopub/build/generated/source/buildConfig/debug/com/twitter/sdk/android/mopub/BuildConfig.java
+++ twitter-mopub/build/generated/source/buildConfig/debug/com/twitter/sdk/android/mopub/BuildConfig.java
@@ -13,6 +13,6 @@ public final class BuildConfig {
   // Fields from build type: debug
   public static final String ARTIFACT_ID = "twitter-mopub";
   public static final String BUILD_NUMBER = "dev";
-  public static final long BUILD_TIME = 1524756949059L;
+  public static final long BUILD_TIME = 1525143159516L;
   public static final String GROUP = "com.twitter.sdk.android";
 }
