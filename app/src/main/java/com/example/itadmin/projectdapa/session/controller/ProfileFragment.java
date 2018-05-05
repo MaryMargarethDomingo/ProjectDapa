e148adfb1e38f30d328725b67e12dbd362887b58
IT Admin <it.admin@iacademy.loc>
2018-05-05 08:27:50 +0800

    fixed bug in logging out

--- app/src/main/java/com/example/itadmin/projectdapa/session/controller/ProfileFragment.java
+++ app/src/main/java/com/example/itadmin/projectdapa/session/controller/ProfileFragment.java
@@ -95,6 +95,7 @@ public class ProfileFragment extends Fragment {
 
 
                 startActivity(new Intent(getActivity(), LoginActivity.class));
+                getActivity().finish();
             }
         });
     }
