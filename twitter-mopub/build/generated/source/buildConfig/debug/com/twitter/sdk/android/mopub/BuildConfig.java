8c8c144bab6c628b64ec1246f59490f699b709e6
anrydomingo <danielabalos1998@gmail.com>
2018-07-26 08:25:25 +0800

    merging

--- twitter-mopub/build/generated/source/buildConfig/debug/com/twitter/sdk/android/mopub/BuildConfig.java
+++ twitter-mopub/build/generated/source/buildConfig/debug/com/twitter/sdk/android/mopub/BuildConfig.java
@@ -13,6 +13,6 @@ public final class BuildConfig {
   // Fields from build type: debug
   public static final String ARTIFACT_ID = "twitter-mopub";
   public static final String BUILD_NUMBER = "dev";
-  public static final long BUILD_TIME = 1532497411020L;
+  public static final long BUILD_TIME = 1532564476241L;
   public static final String GROUP = "com.twitter.sdk.android";
 }
